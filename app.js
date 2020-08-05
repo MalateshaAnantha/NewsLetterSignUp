0883c8ea95f36a3f786800e8f6e995265d3a30fd
Malatesha Anantha <msomasun@usc.edu>
2020-08-04 21:21:42 -0700

    changed success message

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const bodyParser= require('body-parser');
 const request= require('request');
 const https=require('https');
 
-
 const app= express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
